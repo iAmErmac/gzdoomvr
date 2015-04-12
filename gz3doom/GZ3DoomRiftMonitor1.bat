4bd80c88df53d452c7cb0fd3026b8e4bbcb6af08
Christopher Bruns <cmbruns@rotatingpenguin.com>
2015-04-12 10:33:01 -0400

    Correct spelling of "oculardium_optimosa" in Rift shortcuts

--- gz3doom/GZ3DoomRiftMonitor1.bat
+++ gz3doom/GZ3DoomRiftMonitor1.bat
@@ -3,6 +3,6 @@ REM Drag and Drop of .pk3 and PWAD files is supported
 REM Change working folder to .bat location, so we can find gz3doom, even if a file from another folder is dragged onto this .bat
 cd /d "%~dp0"
 REM Place extra arguments first, since that seems to work better for brutal doom pk3 file.
-gz3doom.exe %* +fullscreen 1 +vr_mode 8 -width 1920 -height 1080 +vid_adapter 1 +ocularium_optimosa
+gz3doom.exe %* +fullscreen 1 +vr_mode 8 -width 1920 -height 1080 +vid_adapter 1 +oculardium_optimosa
 REM pause is for debugging
 REM pause
