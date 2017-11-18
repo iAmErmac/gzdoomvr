5102819d3771cf57ea7c9aa2d680dce7aa8f3fde
alexey.lysiuk <alexey.lysiuk@gmail.com>
2017-11-18 13:24:17 +0200

    Implemented setting of window title in Cocoa backend
    # Conflicts:
    #	src/posix/cocoa/i_video.mm

--- src/posix/cocoa/i_video.mm
+++ src/posix/cocoa/i_video.mm
@@ -1552,16 +1552,5 @@ void I_SetMainWindowVisible(bool visible)
 // each platform has its own specific version of this function.
 void I_SetWindowTitle(const char* title)
 {
-	if (title)
-	{
-		static NSString* const TITLE_STRING =
-		[NSString stringWithFormat:@"%s", title];
-		[m_window setTitle:TITLE_STRING];
-	}
-	else
-	{
-		static NSString* const TITLE_STRING =
-		[NSString stringWithFormat:@"%s %s", GAMESIG, GetVersionString()];
-		[m_window setTitle:TITLE_STRING];
-	}
+	CocoaVideo::SetWindowTitle(title);
 }
