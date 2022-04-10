2320bdd0def4e0524a619c2ad8c23a99a205e0d0
Ermac <noobsaibot2k16@gmail.com>
2022-04-11 04:01:31 +0600

    Alternate button mapping for Oculus added
    Alternate button mapping has been added which now matches (almost all buttons but a few) button mapping from QuestZDoom, tested on Rift CV1 and Quest 2. Regular button mapping stays unchanged. Main hand Grip combo is only accessible in alternate mapping mode.

--- src/common/rendering/hwrenderer/data/hw_vrmodes.cpp
+++ src/common/rendering/hwrenderer/data/hw_vrmodes.cpp
@@ -49,6 +49,8 @@ CVAR(Bool, vr_crouch_use_button, true, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 CVAR(Float, vr_vunits_per_meter, 32.0f, CVAR_ARCHIVE | CVAR_GLOBALCONFIG) // METERS
 CVAR(Float, vr_floor_offset, 0.0f, CVAR_ARCHIVE | CVAR_GLOBALCONFIG) // METERS
 CVAR(Bool, openvr_rightHanded, true, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
+CVAR(Bool, vr_use_alternate_mapping, false, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
+CVAR(Bool, vr_secondary_button_mappings, false, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 CVAR(Bool, openvr_moveFollowsOffHand, false, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 CVAR(Bool, vr_teleport, false, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 CVAR(Bool, openvr_drawControllers, false, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
